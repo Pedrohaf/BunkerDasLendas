c171e5e4b1104bbd9a1dedccbfd94d9a49dad264
Pedhaf <pedro22ahferreira@gmail.com>
2023-04-12 18:45:36 -0300

    add pages index and show article

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-
-
-
-    has_many :articles 
+                    has_many :articles
 end
 
