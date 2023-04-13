3cdbebe2ed2202472ca57d18e79c1ed903a41051
Pedhaf <pedro22ahferreira@gmail.com>
2023-04-13 19:08:03 -0300

    add crud articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,5 +4,36 @@ class ArticlesController < ApplicationController
     end
     def show
         @article = Article.find(params[:id])
-      end
-end
+    end
+    def new
+        @article = Article.new
+    end
+    def create
+        @article = Article.new(article_params)
+        @article.user = User.first
+        if @article.save
+          flash[:success] = "Artigo criado com sucesso!"
+          redirect_to article_path(@article)
+        else
+          render 'new'
+        end
+    end
+    def edit
+       @article = Article.find(params[:id])
+    end
+    def update
+        if @article.update(article_params)
+          flash[:success] = "Artigo atualizado com sucesso!"
+          redirect_to article_path(@article)
+        else
+          render 'edit'
+        end
+    end
+      
+    private
+      
+     def article_params
+        params.require(:article).permit(:name, :description)
+     end 
+    
+end     
