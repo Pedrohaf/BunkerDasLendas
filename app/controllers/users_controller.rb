5d8ac44885ecba11b289c9d12106e0d73f5e060a
Pedhaf <pedro22ahferreira@gmail.com>
2023-04-15 08:45:24 -0300

    adiconar edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,51 +1,53 @@
 class UsersController < ApplicationController
+
     def new
         @user = User.new
     end
-    def create
-        @user = User.new(user_params)
-        if @user.save
-          flash[:success] = "Welcome #{@user.username} to MyRecipes App!"
-          redirect_to user_path(@user)
-        else
-          render 'new'
-        end
+    def create 
+      @user = User.new(user_params)
+      if @user.save 
+        flash[:success] = "bem vido #{@user.username} ao Blog das lendas"
+        redirect_to user_path(@user)
+      else
+        render 'new'
+      end    
     end
 
-    def show
-      @users = User.find(params[:id])
+    def show 
+      @user = User.find(params[:id])
+      
     end
+
     def edit
       @user = User.find(params[:id])
     end
-    
+
     def update
       @user = User.find(params[:id])
       if @user.update(user_params)
-        flash[:success] = "Your account was updated successfully"
+        flash[:success] = "Conta atualizada com sucesso"
         redirect_to @user
       else
         render 'edit'
       end  
     end
-
-    def show
-      @user = User.find(params[:id])
-    end
-
-    def index
-      @users = User.all
-    end
-
-    def destroy
-      @user.destroy
-      flash[:danger] = "user and all associated recipes have been deleted"
-      redirect_to users_path
-    end
-    
-      private
+          
+        
       
+  
+    
+    
+    
+
+
+
+
+
+
+    private 
       def user_params
         params.require(:user).permit(:username, :email, :password, :password_confirmation)
+        
       end
+      
 end
