7a0cb75699572d24023c9128d3426002ad2e26ee
Pedhaf <pedro22ahferreira@gmail.com>
2023-04-16 04:46:03 -0300

    add comment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
     end
     
     def show
-
+      @comment = Comment.new
       @comments = @article.comments.paginate(page: params[:page], 
         per_page: 5)
       
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,9 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#delete'
+
+  resources :articles do
+  resources :comments, only: [:create]
   
   
 end
